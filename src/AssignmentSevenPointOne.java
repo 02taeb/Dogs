03d3f50b5b3b1b2ac6f077976b405a20f70a6573
Lucas Ilstedt <lucasilstedt@gmail.com>
2021-12-05 12:25:37 +0100

    Removed deprecated test code

--- src/AssignmentSevenPointOne.java
+++ src/AssignmentSevenPointOne.java
@@ -3,9 +3,5 @@
  * @author Lucas Ilstedt, luil7872
  */
 public class AssignmentSevenPointOne {
-    public static void main(String[] args) {
-        System.out.println("Some changes made");
-        System.out.println("Some more changes made");
-        System.out.println("Even more changes made");
-    }
+    
 }
