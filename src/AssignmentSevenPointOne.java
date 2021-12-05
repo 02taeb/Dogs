87333770343f4dc2923eccdd7439f1516708cca2
Lucas Ilstedt <lucasilstedt@gmail.com>
2021-12-05 12:23:09 +0100

    Testing pull requests and fetch

--- src/AssignmentSevenPointOne.java
+++ src/AssignmentSevenPointOne.java
@@ -6,5 +6,6 @@ public class AssignmentSevenPointOne {
     public static void main(String[] args) {
         System.out.println("Some changes made");
         System.out.println("Some more changes made");
+        System.out.println("Even more changes made");
     }
 }
