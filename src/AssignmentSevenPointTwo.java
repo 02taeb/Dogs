7ab0d612ec07d09e4dffa238202b0e93aabd234e
Lucas Ilstedt <lucasilstedt@gmail.com>
2021-12-05 13:32:26 +0100

    Added Starting Structure

---
+++ src/AssignmentSevenPointTwo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/**
+ * AssignmentSevenPointTwo on iLearn PROG1.
+ * @author Lucas Ilstedt, luil7872
+ */
+ public class AssignmentSevenPointTwo {
+    @UnderTest(id="dogs")
+    private ArrayList<Dog> dogs = new ArrayList<>();
+    private Reader reader = new Reader();
+
+    @UnderTest(id="U7.2")
+    public void handleInput() {
+        
+    }
+
+    private void searchForDogsByTailSize() {
+
+    }
+}
