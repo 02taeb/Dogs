de149fd242d6326e847936ae71fa69546ee4d586
Lucas Ilstedt <lucasilstedt@gmail.com>
2021-12-05 18:15:28 +0100

    AssignmentSevenPointThree 1.0

---
+++ src/AssignmentSevenPointThree.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+/**
+ * AssignmentSevenPointThree on iLearn PROG1.
+ * @author Lucas Ilstedt, luil7872
+ */
+public class AssignmentSevenPointThree {
+    @UnderTest(id="dogs")
+    private ArrayList<Dog> dogs = new ArrayList<>();
+    private Dog nullDog;
+    
+    @UnderTest(id="U7.3")
+    public Dog findDog(String dogName) {
+        nullDog = null;
+        
+        for (Dog dog : dogs) {
+            if (dog.getName().equals(nameCheck(dogName))) {
+                return dog;
+            }
+        }
+        
+        return nullDog;
+    }
+
+    /**
+     * Formats a string, making all characters except the first one lower case and removing spaces at beginning and end.
+     * @param uneditedString String to format.
+     * @return Formatted String.
+     */
+    private String nameCheck(String uneditedString) { 
+        String editedString = uneditedString.toLowerCase();
+        editedString = editedString.trim();
+
+        if (editedString.equals("") || editedString.equals(null)) {
+            return "";
+        }
+
+        editedString = String.valueOf(editedString.charAt(0)).toUpperCase() + editedString.substring(1);
+        return editedString;
+    }
+}
