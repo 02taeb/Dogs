713fa5059a9026f6fb6358accf4e5202864e9dc3
Lucas Ilstedt <lucasilstedt@gmail.com>
2021-12-05 18:31:51 +0100

    AssignmentSevenPointFour 0.1

---
+++ src/AssignmentSevenPointFour.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * AssignmentSevenPointFour on iLearn PROG1.
+ * @author Lucas Ilstedt, luil7872
+ */
+public class AssignmentSevenPointFour {
+    @UnderTest(id="dogs")
+    private ArrayList<Dog> dogs = new ArrayList<>();
+
+    @UnderTest(id="U7.4")
+    public void increaseAge() {
+        
+    }
+}
