40b832b6697508c06e35c8d441afee5b294dd4bf
Lucas Ilstedt <lucasilstedt@gmail.com>
2021-12-09 14:06:28 +0100

    AssignmentSevenPointThree 2.0, AssignmentSevenPointFour 2.0

--- src/AssignmentSevenPointFour.java
+++ src/AssignmentSevenPointFour.java
@@ -2,24 +2,25 @@ import java.util.ArrayList;
 
 /**
  * AssignmentSevenPointFour on iLearn PROG1.
+ * 
  * @author Lucas Ilstedt, luil7872
  */
 public class AssignmentSevenPointFour {
-    //#region Instance variables from AssignmentSevenPointFour
-    @UnderTest(id="dogs")
+    // #region Instance variables U7.4
+    @UnderTest(id = "dogs")
     private ArrayList<Dog> dogs = new ArrayList<>();
     private Reader reader = new Reader();
-    //#endregion
-    //#region Instance variables from AssignmentSevenPointThree
+    // #endregion
+    // #region Instance variables U7.3
     private Dog nullDog;
-    //#endregion
+    // #endregion
 
     /**
-     * Increases the age of dog with name asked for. 
-     * Prints error if no dog with name is found.
+     * Increases the age of Dog with name asked for.
+     * Prints error if no Dog with name is found.
      * Uses findDog(dogName) from AssignmentSevenPointThree.
      */
-    @UnderTest(id="U7.4")
+    @UnderTest(id = "U7.4")
     public void increaseAge() {
         String dogName = reader.readString("Vad heter hunden", false);
         Dog dog = findDog(dogName);
@@ -31,43 +32,25 @@ public class AssignmentSevenPointFour {
             System.out.println(dog.getName() + " är nu ett år äldre.");
         }
     }
-    
-    // #region LOC from AssignmentSevenPointThree
-    // findDog(dogName) changed from public to private in this Assignment.
 
+    // #region LOC from AssignmentSevenPointThree
     /**
      * Tries to find a dog with given name in local ArrayList.
+     * 
      * @param dogName String with dogs name.
      * @return Dog with given name or if none exists Dog with value null.
      */
-    @UnderTest(id="U7.3")
+    @UnderTest(id = "U7.3")
     private Dog findDog(String dogName) {
         nullDog = null;
-        
+
         for (Dog dog : dogs) {
-            if (dog.getName().equals(nameCheck(dogName))) {
+            if (dog.getName().equalsIgnoreCase(dogName.trim())) {
                 return dog;
             }
         }
-        
+
         return nullDog;
     }
-
-    /**
-     * Formats a string, making all characters except the first one lower case and removing spaces at beginning and end.
-     * @param uneditedString String to format.
-     * @return Formatted String.
-     */
-    private String nameCheck(String uneditedString) { 
-        String editedString = uneditedString.toLowerCase();
-        editedString = editedString.trim();
-
-        if (editedString.equals("") || editedString.equals(null)) {
-            return "";
-        }
-
-        editedString = String.valueOf(editedString.charAt(0)).toUpperCase() + editedString.substring(1);
-        return editedString;
-    }
-    //#endregion
+    // #endregion
 }
