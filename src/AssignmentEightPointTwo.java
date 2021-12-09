e9989e975c64a28c28d6730241a2c41d9e338e2b
Lucas Ilstedt <lucasilstedt@gmail.com>
2021-12-09 14:37:12 +0100

    AssignmentEightPointOne 2.0, AssignmentEightPointTwo 2.0

--- src/AssignmentEightPointTwo.java
+++ src/AssignmentEightPointTwo.java
@@ -2,23 +2,26 @@ import java.util.ArrayList;
 
 /**
  * AssignmentEightPointTwo on iLearn PROG1.
+ * 
  * @author Lucas Ilstedt, luil7872
  */
 public class AssignmentEightPointTwo {
-    //#region Instance Variables
-    @UnderTest(id="owners")
+    // #region Instance Variables
+    @UnderTest(id = "owners")
     private ArrayList<Owner> owners = new ArrayList<>();
-    //#endregion
+    // #endregion
 
     /**
      * Finds Owner with given name in local ArrayList<Owner>.
+     * 
      * @param nameOfOwner Name of Owner to search for.
-     * @return Instance of Owner class with given name or Owner with value null if no such owner is found.
+     * @return Instance of Owner class with given name or Owner with value null if
+     *         no such owner is found.
      */
-    @UnderTest(id="U8.2")
+    @UnderTest(id = "U8.2")
     public Owner findOwner(String nameOfOwner) {
         Owner nullOwner = null;
-        
+
         for (int i = 0; i < owners.size(); i++) {
             if (owners.get(i).getName().equalsIgnoreCase(nameOfOwner)) {
                 return owners.get(i);
